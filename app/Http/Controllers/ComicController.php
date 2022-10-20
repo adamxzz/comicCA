e42be55b017c508552a3098ea1b8a838eb469014
adamxzz <adamzaman51@gmail.com>
2022-10-20 10:01:24 +0100

    first commit

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -3,13 +3,33 @@
 namespace App\Http\Controllers;
 
 use App\Http\Resources\ComicCollection;
+use App\Http\Resources\ComicResource;
 use App\Models\Comic;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class ComicController extends Controller
 {
     /**
      * Display a listing of the resource.
+     *
+ * @OA\Get(
+ *     path="/api/comics",
+ *     description="Displays all the comics",
+ *     tags={"Comics"},
+     *      @OA\Response(
+        *          response=200,
+        *          description="Successful operation, Returns a list of Comic in JSON format"
+        *       ),
+        *      @OA\Response(
+        *          response=401,
+        *          description="Unauthenticated",
+        *      ),
+        *      @OA\Response(
+        *          response=403,
+        *          description="Forbidden"
+        *      )
+ * )
      *
      * @return \Illuminate\Http\Response
      */
--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,7 +7,32 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+
+
+ /**
+ * @OA\Info(
+ *      version="2.0.0",
+ *      title="Comic Book API",
+ *      description="AdamZ Comic Book API CA",
+ *      @OA\Contact(
+ *          email="N00191010@iadt.ie"
+ *      ),
+ *     @OA\License(
+ *         name="Apache 2.0",
+ *         url="https://www.apache.org/licenses/LICENSE-2.0.html"
+ *     )
+ * )
+ * @OA\Get(
+ *      path="/",
+ *      description="Home Page",
+ *      @OA\Response(response="default", description="Welcome page")
+ * )
+ */
+
+
 class Controller extends BaseController
 {
+
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
 }
