95d347a9c0c3438deb42dc954844bc2996647716
adamxzz <adamzaman51@gmail.com>
2022-10-27 10:06:25 +0100

    final code without comments

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,6 +7,27 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+ /**
+ * @OA\Info(
+ *      version="2.0.0",
+ *      title="Comic API",
+ *      description="AdamZ Easy Comic API",
+
+ *      @OA\Contact(
+ *          email="N00191010@iadt.ie"
+ *      ),
+ *     @OA\License(
+ *         name="Apache 2.0",
+ *         url="https://www.apache.org/licenses/LICENSE-2.0.html"
+ *     )
+ * )
+ * @OA\Get(
+ *   path="/",
+ *   description="HomePage",
+ *   @OA\Response(response="default", description="Welcome page")
+ * )
+ */
+
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
